314e6646805b7259e8c4b4cb4c051cb09bc1474e
Digvijay Raut <daduraut123@gmail.com>
2025-06-09 12:47:48 -0400

    Time: 326 ms (93.21%) | Memory: 0B (100.00%) - LeetSync

--- QueryQuest/626-exchange-seats/exchange-seats.sql
+++ QueryQuest/626-exchange-seats/exchange-seats.sql
@@ -1,9 +1,9 @@
 # Write your MySQL query statement below
 select
 case
-    when id % 2 = 1 and id + 1 in (select id from seat) then id + 1
-    when id % 2 = 0 then id - 1
-    else id
+    when id % 2 = 1 and id + 1 in (select id from seat) then id + 1. #checks if seat is odd and can be swapped with next even seat if that seat exists in table
+    when id % 2 = 0 then id - 1 # swaps even seat with previous seat
+    else id # when you have odd seats then first and second conditions fail at end of table and we just return same id
 end as id, student
 from seat
 order by id;
