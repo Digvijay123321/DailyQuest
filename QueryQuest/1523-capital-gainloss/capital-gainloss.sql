69c7556dc748a6f38ce9ebfaaf105b8b30aee099
Digvijay Raut <daduraut123@gmail.com>
2025-06-12 17:06:32 -0400

    Time: 465 ms (95.17%) | Memory: 0B (100.00%) - LeetSync

--- QueryQuest/1523-capital-gainloss/capital-gainloss.sql
+++ QueryQuest/1523-capital-gainloss/capital-gainloss.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 select stock_name, 
-    sum(case when operation = "Sell" then price else 0 end) - sum(case when operation = "Buy" then price else 0 end) as capital_gain_loss
+    sum(case when operation = "Sell" then price else -1*price end) as capital_gain_loss
 from stocks
 group by stock_name
