4148cd3bdd43727761e5748657952784ef0ef6d9
Digvijay Raut <daduraut123@gmail.com>
2025-06-12 13:22:33 -0400

    Time: 1114 ms (95.38%) | Memory: 0B (100.00%) - LeetSync

---
+++ QueryQuest/1480-movie-rating/movie-rating.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+with name as (
+    select u.name, count(*) as count_of_rateings
+    from movierating as m
+    left join users as u on u.user_id = m.user_id
+    group by m.user_id
+    order by count_of_rateings desc, u.name
+    limit 1
+), movie as (
+    select m.title, avg(rating) as avg_rate
+    from movierating as mr
+    left join movies as m on m.movie_id = mr.movie_id
+    where year(created_at) = 2020 and month(created_at) = 02
+    group by mr.movie_id
+    order by avg_rate desc, m.title
+    limit 1
+)
+select name as results from name
+union all
+select title from movie
+
+
+
